69a80770eed44a45882852a5d1f1d3ca8d96c947
afiqkembar <kembar9611@yahoo.com>
2017-07-27 16:12:09 +0800

    new

---
+++ clock.js
@@ -0,0 +1,15 @@
+function clock(time)
+{
+  var split = time.split(":");
+var first = parseInt(split[0]);
+var ampm = time.substring(8,10);
+
+if(ampm == "PM"){
+ document.write(first + ":" + split[1] + ":" + time[6]+time[7]);
+}
+else{
+  first = "00";
+  document.write(first+":"+split[1]+":"+time[6]+time[7]);
+}
+}
+clock("08:45:00AM");
